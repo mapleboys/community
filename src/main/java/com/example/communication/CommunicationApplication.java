12ffc5cd6f4107e85d57ad0efa3b2ec2a7c231ab
mapleboys <guyang1125@163.com>
2022-01-05 21:46:07 +0800

    fix data error20220105

--- src/main/java/com/example/communication/CommunicationApplication.java
+++ src/main/java/com/example/communication/CommunicationApplication.java
@@ -3,9 +3,12 @@ package com.example.communication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("com.example.communication.mapper")
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class CommunicationApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/db/migration/V5__update_creator_on_question.sql
@@ -0,0 +1,1 @@
+update question set creator = '22';
