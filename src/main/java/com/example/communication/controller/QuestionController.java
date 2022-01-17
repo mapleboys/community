770b66fb888909a6a25fd411fa8ca0a40844be88
mapleboys <guyang1125@163.com>
2022-01-17 15:46:23 +0800

    add view count

--- src/main/java/com/example/communication/controller/QuestionController.java
+++ src/main/java/com/example/communication/controller/QuestionController.java
@@ -2,6 +2,8 @@ package com.example.communication.controller;
 
 import com.example.communication.dto.PaginationDto;
 import com.example.communication.dto.QuestionDto;
+import com.example.communication.mapper.QuestionExtMapper;
+import com.example.communication.model.Question;
 import com.example.communication.service.QuestionDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,13 +16,22 @@ public class QuestionController {
     @Autowired
     QuestionDtoService questionDtoService;
 
+    @Autowired
+    QuestionExtMapper questionExtMapper;
+
     @GetMapping("/question/{id}")
     public String QuestionDetail(@PathVariable(name = "id") Integer id,
                                  Model model) {
         QuestionDto question = questionDtoService.selectById(id);
         model.addAttribute("question", question);
-
+        incViewNum(id);
         return "questionDetail";
+    }
+
+    private void incViewNum(Integer id) {
+        Question question = new Question();
+        question.setId(id);
+        questionExtMapper.incView(question);
     }
 
 
